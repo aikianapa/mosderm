a62156819dd8d407c69a3c9ac3aa88e3d2ed3006
Oleg Frolov <oleg_frolov@mail.ru>
2019-10-04 17:12:58 +0300

    update

--- forms/branches/branches_edit.php
+++ forms/branches/branches_edit.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="{{_form}}_{{_mode}}" data-keyboard="false" data-backdrop="true" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
+  <div class="modal-dialog modal-xl" role="document">
 	<div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -32,7 +32,7 @@
 <ul class="nav nav-tabs" role="tablist">
 	<li class="nav-item"><a class="nav-link active" href="#{{_form}}Descr" data-toggle="tab">Свойства</a></li>
 	<li class="nav-item"><a class="nav-link" href="#{{_form}}Text" data-toggle="tab" >Описание</a></li>
-	<!--li class="nav-item"><a class="nav-link" href="#{{_form}}ource" data-toggle="tab" >{{_lang[source]}}</a></li-->
+	<li class="nav-item"><a class="nav-link" href="#{{_form}}Schedule" data-toggle="tab" >Расписание</a></li>
 	<li class="nav-item"><a class="nav-link" href="#{{_form}}Images" data-toggle="tab">Изображения</a></li>
     <li class="nav-item"><a class="nav-link" href="#{{_form}}seo" data-toggle="tab">СЕО</a></li>
 </ul>
@@ -97,7 +97,38 @@
     <div class="col-sm-2"><label class="switch switch-success"><input type="checkbox" name="active"><span></span></label></div>
 	</div>
 </div>
-
+<div id="{{_form}}Schedule" class="tab-pane fade" role="tabpanel">
+<p>Расписание приёма граждан по личным вопросам</p>
+    <div data-wb-role="multiinput" name="schedule">
+        <div class="col-4">
+            <label class="form-control-label">Адрес приёмной</label>
+            <textarea class="summernote" name="address" data-height="90" 
+            data-toolbar="[['style', ['bold', 'italic', 'codeview', 'clear']]]"></textarea>
+        </div>
+        <div class="col-4">
+            <label class="form-control-label">Время приёма</label>
+            <textarea class="summernote" name="address" data-height="90" 
+            data-toolbar="[['style', ['bold', 'italic', 'codeview', 'clear']]]"></textarea>
+        </div>
+        <div class="col mt-4">
+            <div class="row">
+                <div class="col">
+                    <label class="form-control-label">Телефон для записи</label>
+                    <input type="phone" class="form-control" name="phone" placeholder="Телефон для записи">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <label class="form-control-label">Кто принимает</label>
+                   <select class="form-control select2" name="boss" placeholder="Кто принимает..." data-wb-role="foreach" data-wb-form="specialists" data-wb-where='branch = "{{_item}}"' data-wb-tpl="false" data-wb-sort="name">
+                        <option value="{{id}}">{{name}}</option>
+                   </select>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
 <div id="{{_form}}Text" class="tab-pane fade" data-wb-role="include" src="editor" role="tabpanel"></div>
 <div id="{{_form}}Images" class="tab-pane fade" data-wb-role="include" src="uploader" role="tabpanel"></div>
 <div id="{{_form}}seo" class="tab-pane fade" data-wb-role="include" src="seo" role="tabpanel"></div>
