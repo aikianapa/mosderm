8f340abda4fa45e10ee4311024707158b75b1ab0
Blacker <oleg_frolov@mail.ru>
2019-11-25 14:05:00 +0300

    widget spec

--- tpl/section.widget.spec.inc.php
+++ tpl/section.widget.spec.inc.php
@@ -2,12 +2,15 @@
         <!-- tpl/section.widget.spec.inc.php -->
         <div class="container" data-wb-role="formdata" data-wb-form="branches" data-wb-item="{{office}}" data-wb-json='{"office":"{{office}}"}'  data-wb-hide="wb">
             <h2>Наши специалисты <a href="/specialists">Все специалисты</a></h2>
-            
+
             <meta data-wb-role="variable" var="where" data-wb-if='"{{_form}}"="branches" AND "{{_mode}}"="show"' value=' AND branch = "{{_item}}"' else='AND widget="on" '>
             <meta data-wb-role="variable" var="where" data-wb-if='"{{office}}">""' value=' AND office = "{{office}}" AND ( widget="" OR widget="on" ) ' else='{{_var.where}}'>
             <meta data-wb-role="variable" var="where" data-wb-if='"{{_form}}"="branches" AND "{{_mode}}"="show" AND type = "office"' value=' AND office = "{{_item}}"' else='{{_var.where}}'>
             <meta data-wb-role="variable" var="where" data-wb-if='{{strpos(strtolower({{name}}),"платн")}}>0 AND "{{_mode}}"="show" AND type = "office"' value='{{_var.where}} OR (active="on" AND branch = "{{main}}" AND commerce = "on" )' else='{{_var.where}}'>
-            <div class="our-specialists__slider" data-wb-role="foreach" data-wb-tpl="false" data-wb-where='active="on" {{_var[where]}}' data-wb-limit="10" data-wb-rand="true" data-wb-form="specialists" data-wb-hide="wb">
+
+            <meta data-wb-role="variable" var="limit" data-wb-if='"{{_item}}" = "home"' value="10" else=''>
+
+            <div class="our-specialists__slider" data-wb-role="foreach" data-wb-tpl="false" data-wb-where='active="on" {{_var[where]}}' data-wb-limit="{{_var.limit}}" data-wb-rand="true" data-wb-form="specialists" data-wb-hide="wb">
                 <div class="our-specialist__item">
                     <a href="/specialists/{{id}}/{{wbFurlGenerate({{name}})}}"></a>
                     <div class="our-specialist__slider-wrap">
@@ -20,7 +23,7 @@
                             <p>{{descr}}</p>
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
              <meta data-wb-selector=".our-specialists__slider [data-src]" data-wb-removeattr="data-src">
