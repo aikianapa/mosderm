45ee7f1ba9c693e41377a6656c943740507ceb61
Blacker <oleg_frolov@mail.ru>
2019-11-26 21:11:54 +0300

    update

--- tpl/section.widget.spec.inc.php
+++ tpl/section.widget.spec.inc.php
@@ -8,14 +8,14 @@
             <meta data-wb-role="variable" var="where" data-wb-if='"{{_form}}"="branches" AND "{{_mode}}"="show" AND type = "office"' value=' AND office = "{{_item}}"' else='{{_var.where}}'>
             <meta data-wb-role="variable" var="where" data-wb-if='{{strpos(strtolower({{name}}),"платн")}}>0 AND "{{_mode}}"="show" AND type = "office"' value='{{_var.where}} OR (active="on" AND branch = "{{main}}" AND commerce = "on" )' else='{{_var.where}}'>
 
-            <meta data-wb-role="variable" var="limit" data-wb-if='"{{_item}}" = "home"' value="10" else=''>
+            <meta data-wb-role="variable" var="limit" data-wb-if='"{{_form}}" = "branches" AND "{{_mode}}" = "show"' value="" else='10'>
 
             <div class="our-specialists__slider" data-wb-role="foreach" data-wb-tpl="false" data-wb-where='active="on" {{_var[where]}}' data-wb-limit="{{_var.limit}}" data-wb-rand="true" data-wb-form="specialists" data-wb-hide="wb">
                 <div class="our-specialist__item">
                     <a href="/specialists/{{id}}/{{wbFurlGenerate({{name}})}}"></a>
                     <div class="our-specialist__slider-wrap">
                         <div class="our-specialist__img">
-                            <img data-wb-role="thumbnail" data-wb-size="200px;260px;src" src="/uploads/{{_table}}/{{id}}/{{image[0].img}}" alt="{{name}} - {{spec}}">
+                            <img data-wb-role="thumbnail" data-wb-size="200px;200px;src" offset="0;5" src="/uploads/{{_table}}/{{id}}/{{image[0].img}}" alt="{{name}} - {{spec}}">
                         </div>
                         <div class="our-specialist__text">
                             <p><strong>{{name}}</strong></p>
