d0208dc33d908d80363d509cc5be8c2b09e48214
Blacker <oleg_frolov@mail.ru>
2019-12-10 09:19:33 +0300

    update

--- tpl/section.widget.spec.inc.php
+++ tpl/section.widget.spec.inc.php
@@ -3,6 +3,7 @@
         <div class="container" data-wb-role="formdata" data-wb-form="branches" data-wb-item="{{office}}" data-wb-json='{"office":"{{office}}"}'  data-wb-hide="wb">
             <h2>Наши специалисты <a href="/specialists">Все специалисты</a></h2>
 
+            <meta data-wb-role="variable" var="where" data-wb-if='"{{_form}}"="pages" AND "{{_item}}"="home"' value=' AND "{{image[0][img]}}" > "" ' else=' AND widget="on" '>
             <meta data-wb-role="variable" var="where" data-wb-if='"{{_form}}"="branches" AND "{{_mode}}"="show"' value=' AND branch = "{{_item}}"' else='AND widget="on" '>
             <meta data-wb-role="variable" var="where" data-wb-if='"{{office}}">""' value=' AND office = "{{office}}" AND ( widget="" OR widget="on" ) ' else='{{_var.where}}'>
             <meta data-wb-role="variable" var="where" data-wb-if='"{{_form}}"="branches" AND "{{_mode}}"="show" AND type = "office"' value=' AND office = "{{_item}}"' else='{{_var.where}}'>
@@ -10,7 +11,7 @@
 
             <meta data-wb-role="variable" var="limit" data-wb-if='"{{_form}}" = "branches" AND "{{_mode}}" = "show"' value="" else='10'>
 
-            <div class="our-specialists__slider" data-wb-role="foreach" data-wb-tpl="false" data-wb-where='active="on" AND "{{image[0][img]}}" > "" {{_var[where]}}' data-wb-limit="{{_var.limit}}" data-wb-rand="true" data-wb-form="specialists" data-wb-hide="wb">
+            <div class="our-specialists__slider" data-wb-role="foreach" data-wb-tpl="false" data-wb-where='active="on" {{_var[where]}}' data-wb-limit="{{_var.limit}}" data-wb-rand="true" data-wb-form="specialists" data-wb-hide="wb">
                 <div class="our-specialist__item">
                     <a href="/specialists/{{id}}/{{wbFurlGenerate({{name}})}}"></a>
                     <div class="our-specialist__slider-wrap">
