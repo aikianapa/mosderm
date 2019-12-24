d23b74b8730ff529bff2cdb553209beace735617
Blacker <oleg_frolov@mail.ru>
2019-12-24 12:14:37 +0300

    update

--- tpl/scripts.inc.php
+++ tpl/scripts.inc.php
@@ -5,7 +5,7 @@
 
 <script src="js/jquery.fancybox.min.js"></script>
 <script src="js/stacktable.js"  data-wb-append="body"></script>
-<script src="js/script.js"  data-wb-append="body"></script>
 <script src="/engine/js/plugins/plugins.js" data-wb-append="body"></script>
 <script src="/engine/js/wbengine.js" data-wb-append="body"></script>
 <script src="https://lidrekon.ru/slep/js/uhpv-full.min.js"></script>
+<script src="js/script.js"  data-wb-append="body"></script>
