ed907f5ce55c9aa4b4b2692c3335c455570d3635
Oleg Frolov <oleg_frolov@mail.ru>
2019-12-26 21:47:18 +0300

    изменено:      null 	изменено:      robots.txt 	изменено:      tpl/head.inc.php

--- tpl/head.inc.php
+++ tpl/head.inc.php
@@ -14,11 +14,39 @@
    ym(48155438, "init", {
         clickmap:true,
         trackLinks:true,
-        accurateTrackBounce:true
+        accurateTrackBounce:true,
+        webvisor:true
    });
 </script>
 <noscript><div><img src="https://mc.yandex.ru/watch/48155438" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
+<script type="text/javascript">// <![CDATA[
+(function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter43819714 = new Ya.Metrika({
+                   id:43327374,
+clickmap:true,
+trackLinks:true,
+accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+// ]]></script>
+
+<noscript><div><img src="https://mc.yandex.ru/watch/43819714" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <link href="https://fonts.googleapis.com/css?family=Rubik&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/slick.css">
     <link rel="stylesheet" href="css/stacktable.css">
